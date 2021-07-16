1b52adf2637967bbd955b254d5363614ed723aac
ByMaglite <fceyhan55@gmail.com>
2021-07-16 08:33:19 +0300

    Validate function bug corrected

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ function validateGuess(guess){
     } else if (guess < 1) {
         alert('Please enter a number greater than 1!');
     } else if (guess > 100){
-        alert('Please enter a number less than 500!')
+        alert('Please enter a number less than 100!')
     } else {        
         previousGuesses.push(guess);
         if (numGuesses === 11){
